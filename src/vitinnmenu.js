020da5b86ddc305559b6fd2062bd173a75a16884
VitorOlive <76225774+VitorOlive@users.noreply.github.com>
2021-01-19 21:17:16 -0300

    Add files via upload

---
+++ src/vitinnmenu.js
@@ -0,0 +1,12 @@
+const vitinnmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Vitinn*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.vitinnmenu = vitinnmenu
